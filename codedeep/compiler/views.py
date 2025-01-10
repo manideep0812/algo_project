0ffe0b05612617615179ebc921e4ee635c6aa832
manideep0812 <narahari.manideep1@gmail.com>
2025-01-10 23:28:57 +0530

    minor adjustments in frontend

--- codedeep/compiler/views.py
+++ codedeep/compiler/views.py
@@ -22,60 +22,6 @@ def submit(request):
         context = {"form" : form,}
         return render(request,"index.html",context)
 
-'''def runCode(language,code,inputData):
-    projectPath = Path(settings.BASE_DIR)
-    directories = ["codes","inputs","outputs"]
-    for directory in directories:
-        dirPath = projectPath/directory
-        if not dirPath.exists():
-            dirPath.mkdir(parents=True,exist_ok=True)
-    codesDir = projectPath/"codes"
-    inputsDir = projectPath/"inputs"
-    outputsDir = projectPath/"outputs"
-
-    unique = str(uuid.uuid4())
-
-    codeFileName = f"{unique}.{language}"
-    inputFileName = f"in_{unique}.txt"
-    outputFileName = f"out_{unique}.txt"
-
-    codeFilePath = codesDir / codeFileName
-    inputFilePath = inputsDir / inputFileName
-    outputFilePath = outputsDir / outputFileName
-
-    with open(codeFilePath,"w") as codeFile:
-        codeFile.write(code)
-    with open(inputFilePath,'w') as inputFile:
-        inputFile.write(inputData)
-    with open(outputFilePath,"w") as outputFile:
-        pass # here we are creating an empty file
-
-    if language == "cpp" or language == "c":
-        executablePath = codesDir/unique
-        compileResult = subprocess.run(
-            ["gcc",str(codeFilePath),"-o",str(executablePath)]
-        )
-        if compileResult.returncode == 0:
-            with open(inputFilePath,'r') as inputText:
-                with open(outputFilePath,"w") as outputText:
-                    subprocess.run(
-                        [str(executablePath)],
-                        stdin=inputText,
-                        stdout=outputText,
-                    )
-    elif language == "py":
-        with open(inputFilePath,'r') as inputText:
-            with open(outputFilePath,'w') as outputText:
-                subprocess.run(
-                    ["python",str(codeFilePath)],
-                    stdin=inputText,
-                    stdout=outputText,
-                )
-    
-    with open(outputFilePath,'r') as outputFile:
-        outputData = outputFile.read()
-    
-    return outputData'''
 
 def runCode(language, code, inputData):
     projectPath = Path(settings.BASE_DIR)
--- codedeep/problems/urls.py
+++ codedeep/problems/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path,include
+'''from django.urls import path,include
 from problems.views import all_problems,problem_detail,home_page,contests
 urlpatterns = [
     path('',home_page),
     path('contests/',contests),
     path('problems/',all_problems),
     path('problems/<int:id>/',problem_detail),
+]'''
+from django.urls import path
+from problems.views import all_problems, problem_detail, home_page, contests
+
+urlpatterns = [
+    path('', home_page, name='home'),
+    path('contests/', contests, name='contests'),
+    path('problems/', all_problems, name='all_problems'),
+    path('problems/<int:id>/', problem_detail, name='problem_detail'),
 ]
